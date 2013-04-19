ec1538482b6b5b49fcae3fb38a3ded932e418d30
Weston Platter <weston.platter@novu.com>
2013-04-19 17:35:51 -0500

    got the algorithm working

--- solution.rb
+++ solution.rb
@@ -12,22 +12,25 @@ File.open("./small_words").each_line do |line|
   data << parsed_line if (parsed_line.length > 5 and parsed_line != last_parsed_line)
   last_parsed_line = parsed_line
 end
-puts data
 
 ### recursive method
 def recursive_chain(data, word)
   keep_going = true
   chain = []
+  chain << word
 
   while keep_going
-    total_chars = word.length
-    exp = Regexp.new("\A#{word[(total_chars-6)..(total_chars)]}")
+    total_chars = word.length+1
+    last_six = word[(total_chars-5)..(total_chars)]
+    # puts "l-#{last_six}"
+    exp = Regexp.new("^#{last_six}")
 
     data.each do |compared_word|
       first_six = compared_word[0..6]
+      # puts "f-#{first_six}"
       if (match = first_six.match(exp))
-        puts match
-        chain << compared_word
+        chain.push(compared_word)
+        puts "chain = #{chain}"
         keep_going = true
         word = compared_word
       else
@@ -35,6 +38,9 @@ def recursive_chain(data, word)
       end
     end
   end
+
+  # puts "\n"
+
 
   if chain.length > 1
     return chain
@@ -47,9 +53,7 @@ end
 ### main iterator
 data.each_with_index do |word, index|
   t = recursive_chain(data, word)
-  puts t if t
-  chains << t if t
-  puts index if index % 1000 == 0
+  chains.push(t) if t
 end
 
 
