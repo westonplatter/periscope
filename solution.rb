c3ccec7b46c5daef64ece135c9d8fd93fca738c9
Weston Platter <weston.platter@novu.com>
2013-04-20 23:52:24 -0500

    final solution. the Lord is merciful and gracious, slow to anger and abounding in steadfast love.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,15 @@
+# Programming Quiz for Periscope
+# Weston Platter (westonplatter@gmail.com)
+#
+
 chain_store = []
 data_subsets = {}
+
 load_counter = 0
-CUT = 6
+CUT_WORD_LENGTH = 6
+
+time_start = Time.new
+
 
 ### Load Strings greater than 5 characters into Hash organized array subsets, IE,
 #
@@ -17,7 +25,7 @@ File.open("./words").each_line do |line|
   # do not pay attention to uppercase/downcase
   parsed_line = line.strip.downcase
 
-  if parsed_line.length > (CUT-1)
+  if parsed_line.length > (CUT_WORD_LENGTH-1)
     hash_key = parsed_line.match(/.../).to_s
     data_subsets["#{hash_key}"] = [] unless data_subsets["#{hash_key}"]
 
@@ -27,14 +35,14 @@ File.open("./words").each_line do |line|
     load_counter += 1
   end
 end
-puts load_counter
-puts data_subsets.count
+puts "words = #{load_counter}"
+puts "Hash contains #{data_subsets.count} array subsets\n\n"
 
 
-### get words in chain for a word
+### For a given word, get next words in chain. Returns empty array of no next words.
 def next_words(word, data_subsets)
   total_chars = word.length
-  last_six = word[(total_chars - CUT)..(total_chars)]
+  last_six = word[(total_chars - CUT_WORD_LENGTH)..(total_chars)]
   exp = Regexp.new("^#{last_six}")
 
   result = []
@@ -54,12 +62,11 @@ def next_words(word, data_subsets)
 end
 
 
-### main iterator
-hh = 0
-max = 0
-transfer = []
+### Main iterator
+iterations = 0
+maxes = []
+
 while chain_store.count > 1
-  time_start = Time.new
 
   chain_store.each_with_index do |chain_store_element, index|
     word = chain_store_element.last
@@ -68,41 +75,24 @@ while chain_store.count > 1
     if next_chain_words.count > 0
       next_chain_words.each do |chain_word|
         ne = Array.new(chain_store_element) << chain_word
-        # puts "#{ne.count} #{chain_word} - #{ne.to_s}" if ne.count > 8
-        puts "#{ne.count} - #{ne.to_s}" if ne.count > 8
         chain_store << ne
 
-        if chain_store_element.count >= max
-          max = chain_store_element.count
-          if max > 3
-            File.open("max_#{max}.txt", "w+") do |file|
-              chain_store.each do |word_chain|
-                file.write "#{word_chain.to_s}\n"
-              end
-            end
-          end
-        end
+        # decided to go fast and hard code when to record maxes.
+        # this could be made to dynamically capture new maxes if needed.
+        maxes << ne if ne.count > 8
       end
     end
 
+    # since we want this to be fast ...
+    # delete (array element) since we added (array element + next chain word)
     chain_store.delete_at(index)
   end
 
-
-
-
-  # puts "# word chains = #{chain_store.count}"
-  # puts "time split = #{Time.new - time_start}"
-  # puts "\n"
+  puts "iteration = #{iterations}, word chains = #{chain_store.count}"
+  iterations += 1
 end
 
-
-### write results to file
-# File.open("small_results.txt", "w+") do |file|
-#   file.write("-- results\n")
-#   chain_store.each do |word_chain|
-#     file.write "#{word_chain.to_s}\n"
-#   end
-
-#   puts "\n\n"
-# end
+puts "\nTime to run = #{Time.new - time_start}\n\n"
+maxes.each do |max|
+  puts "#{max}"
+end
